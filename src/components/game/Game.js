53f49e42b296dd8b6ec568fccdc2f7fa5cede937
Brendan Beltz <bbeltz@sapient.com>
2019-06-14 11:00:25 -0400

    Set up raymarcher dependency as local symlink

---
+++ src/components/game/Game.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WorldEngine from 'raymarcher';
+
+const Game = () => {
+  return (
+    <canvas id="main"></canvas>
+  )
+};
+
+export default Game;
--- src/components/preview-pane/PreviewPane.js
+++ src/components/preview-pane/PreviewPane.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Game from '../game';
 import './PreviewPane.css';
 
-const PreviewPane = () => {
+// MVP We only want to instantiate the game if a WAD is available.
+const PreviewPane = props => {
   return (
-    <div class="preview-pane__container">Preview Pane</div>
+    <div class="preview-pane__container">
+      {
+        props.isWADAvailable
+          ? <Game />
+          : <h1>No WAD is available to load.</h1>
+      }
+    </div>
   )
 };
 
