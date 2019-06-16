bee8c885d9c456ddcab467babbd34b06ecf8ab74
Brendan Beltz <bbeltz@sapient.com>
2019-06-15 21:47:27 -0400

    Allow game to receive focus

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -3,20 +3,18 @@ import WorldEngine from 'raymarcher';
 import './Game.css';
 
 const Game = () => {
-  // const canvasRef = React.useRef(null);
-  // console.log(canvasRef);
   const renderWorld = WorldEngine({
     displayId: "raymarcher-main-display",
     width: 700,
     height: 400,
+    editorMode: true,
   });
   const world = renderWorld();
   return (
-    <>
-      <canvas
-        id="raymarcher-main-display"
-      ></canvas>
-    </>
+    <canvas
+      id="raymarcher-main-display"
+      tabIndex="1"
+    ></canvas>
   )
 };
 
