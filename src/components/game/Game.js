cd61dc51853fba6b17216794c8f43875307811bf
Brendan Beltz <bbeltz@sapient.com>
2019-06-16 00:34:55 -0400

    Start game with local WAD

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import WorldEngine from 'raymarcher';
+import { useSelector } from 'react-redux';
 import './Game.css';
 
 const Game = () => {
-  const renderWorld = WorldEngine({
-    displayId: "raymarcher-main-display",
-    width: 700,
-    height: 400,
-    editorMode: true,
-  });
-  const world = renderWorld();
+  const WAD = useSelector(store => store.level);
+  // TODO: Validate WAD a bit more rigorously
+  if(WAD){
+    const renderWorld = WorldEngine({
+      displayId: "raymarcher-main-display",
+      width: 700,
+      height: 400,
+      editorMode: true,
+    });
+    const world = renderWorld(WAD);
+  }
   return (
     <canvas
       id="raymarcher-main-display"
