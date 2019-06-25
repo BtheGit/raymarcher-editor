656527f2f81e6bc35d2a4c37444b300cfa5c095a
BtheGit <brendanwebdev@gmail.com>
2019-06-25 11:30:27 -0400

    Add floor and ceiling texture selecting

--- src/components/cell-editor/floor-editor/FloorEditor.js
+++ src/components/cell-editor/floor-editor/FloorEditor.js
@@ -12,10 +12,15 @@ const DEFAULT_TEXTURE_CONFIGS = {
   }
 }
 
-const FloorEditor = ({ cellCoords, cellProperties, updateCellProperties, textures }) => {
-  const floorTextureType = cellProperties.textureType;
+const DEFAULT_CEILING_CONFIG = {
+  textureType: 'color',
+  textureConfig: DEFAULT_TEXTURE_CONFIGS.color,
+}
 
+const FloorEditor = ({ cellCoords, cellProperties, updateCellProperties, textureList }) => {
+  const floorTextureType = cellProperties.textureType;
   // TODO: Make custom hook instead of wrapper function. Make it reusable for all cell properties
+
   const handleFloorColorChange = e => {
     const color = e.target.value;
     // TODO: Until we standardize the color scheme, we'll drop the leading hash. DIRTY!
@@ -30,6 +35,20 @@ const FloorEditor = ({ cellCoords, cellProperties, updateCellProperties, texture
     updateCellProperties(cellCoords, newCellConfig);
   };
 
+  const handleFloorImageChange = e => {
+    const name = e.target.value;
+    // TODO: Until we standardize the color scheme, we'll drop the leading hash. DIRTY!
+    const newCellConfig = {
+      ...cellProperties,
+      textureType: 'image',
+      textureConfig: {
+        ...cellProperties.textureConfig,
+        name,
+      }
+    }
+    updateCellProperties(cellCoords, newCellConfig);
+  }
+
   const handleFloorTextureTypeSwitch = e => {
     // No validation, but must be either 'color', 'image', or (eventually) 'gradient'.
     const textureType = e.target.name;
@@ -41,40 +60,188 @@ const FloorEditor = ({ cellCoords, cellProperties, updateCellProperties, texture
     updateCellProperties(cellCoords, newCellConfig);
   }
 
+  // TODO: The following should be able to be rolled into the above functions.
+  const handleCeilingColorChange = e => {
+    const color = e.target.value;
+    // TODO: Until we standardize the color scheme, we'll drop the leading hash. DIRTY!
+    const colorMinusHash = color.substr(1);
+    console.log(colorMinusHash)
+    const newCellConfig = {
+      ...cellProperties,
+      ceilingConfig: {
+        textureType: 'color',
+        textureConfig: {
+          colorType: 'hex',
+          color: colorMinusHash,
+        }
+      }
+    }
+    updateCellProperties(cellCoords, newCellConfig);
+  };
+
+  const handleCeilingImageChange = e => {
+    const name = e.target.value;
+    console.log(name)
+    // TODO: Until we standardize the color scheme, we'll drop the leading hash. DIRTY!
+    const newCellConfig = {
+      ...cellProperties,
+      ceilingConfig: {
+        textureType: 'image',
+        textureConfig: {
+          name,
+        }
+      }
+    }
+    updateCellProperties(cellCoords, newCellConfig);
+  }
+
+  const handleCeilingTextureTypeSwitch = e => {
+    // No validation, but must be either 'color', 'image', or (eventually) 'gradient'.
+    const textureType = e.target.name;
+    const newCellConfig = {
+      ...cellProperties,
+      ceilingConfig: {
+        textureType,
+        textureConfig: DEFAULT_TEXTURE_CONFIGS[textureType],
+      },
+    };
+    updateCellProperties(cellCoords, newCellConfig);
+  }
+
+
+  const handleCeilingToggle = e => {
+    const checked = e.target.checked;
+    if(!checked){
+      const { ceilingConfig, ...rest } = cellProperties;
+      updateCellProperties(cellCoords, rest);
+    }
+    else {
+      const newCellConfig = {
+        ...cellProperties,
+        ceilingConfig: DEFAULT_CEILING_CONFIG,
+      }
+      updateCellProperties(cellCoords, newCellConfig);
+    }
+  }
+
+  const renderCeilingEditor = (ceilingConfig) => {
+    if(!ceilingConfig) {
+      return null;
+    }
+    const ceilingTextureType = ceilingConfig.textureType;
+    console.log(ceilingTextureType)
+    return (
+      <>
+        <div className={ `floor-editor__option-box ${ ceilingTextureType === 'color' ? 'floor-editor__option-box--active' : '' }` }>
+          <form>
+            <input 
+              type="radio" 
+              name="color" 
+              id="ceiling_texture_type__color"
+              checked={ ceilingTextureType === 'color' }
+              onChange={ handleCeilingTextureTypeSwitch }
+            />
+            <label htmlFor="ceiling_texture_type__color">Color</label>
+            {/* I'm assuming hex here and that it lacks a hash. Bad! */}
+            <input 
+              type="color" 
+              name="ceiling_color_picker"
+              value={ `#${ ceilingConfig.textureConfig.color }` } 
+              onChange={ handleCeilingColorChange }
+              disabled={ ceilingTextureType !== 'color' }
+            ></input>
+          </form>
+        </div>
+        <div className={ `floor-editor__option-box ${ ceilingTextureType === 'image' ? 'floor-editor__option-box--active' : '' }` }>
+          <form>
+            <input 
+              type="radio" 
+              name="image" 
+              id="ceiling_texture_type__texture" 
+              checked={ ceilingTextureType === 'image' }
+              onChange={ handleCeilingTextureTypeSwitch }
+            />
+            <label htmlFor="ceiling_texture_type__texture">Image</label>
+    
+            <select
+              disabled={ ceilingTextureType !== 'image' }
+              value={ ceilingConfig.textureConfig.name || 'default' }
+              onChange={ handleCeilingImageChange }
+            >
+              <option value="default">Default</option>
+              {
+                textureList.map((texture, i) => (
+                  <option value={ texture } key={ i }>{ texture }</option>
+                ))
+              }
+            </select>
+          </form>
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className="floor-editor__container">
       <h4>Floor</h4>
-      <input 
-        type="radio" 
-        name="color" 
-        id="floor_texture_type__color" 
-        checked={ floorTextureType === 'color' }
-        onChange={ handleFloorTextureTypeSwitch }
-      />
-      <label htmlFor="floor_texture_type__color">Color</label>
+      <div className={`floor-editor__option-box ${ floorTextureType === 'color' ? 'floor-editor__option-box--active' : '' }`}>
+        <form>
+          <input 
+            type="radio" 
+            name="color" 
+            id="floor_texture_type__color"
+            checked={ floorTextureType === 'color' }
+            onChange={ handleFloorTextureTypeSwitch }
+          />
+          <label htmlFor="floor_texture_type__color">Color</label>
+          {/* I'm assuming hex here and that it lacks a hash. Bad! */}
+          <input 
+            type="color" 
+            name="floor_color_picker"
+            value={ `#${cellProperties.textureConfig.color}` } 
+            onChange={ handleFloorColorChange }
+            disabled={ floorTextureType !== 'color' }
+          ></input>
+        </form>
+      </div>
+      <div className={ `floor-editor__option-box ${ floorTextureType === 'image' ? 'floor-editor__option-box--active' : '' }` }>
+        <form>
+          <input 
+            type="radio" 
+            name="image" 
+            id="floor_texture_type__texture" 
+            checked={ floorTextureType === 'image' }
+            onChange={ handleFloorTextureTypeSwitch }
+          />
+          <label htmlFor="floor_texture_type__texture">Image</label>
 
-      {/* <label htmlFor="floor_color_picker">Color</label> */}
-      {/* I'm assuming hex here and that it lacks a hash. Bad! */}
-      <input type="color" name="floor_color_picker" value={ `#${cellProperties.textureConfig.color}` } onChange={ handleFloorColorChange }></input>
-
-      <input 
-        type="radio" 
-        name="image" 
-        id="floor_texture_type__texture" 
-        checked={ floorTextureType === 'image' }
-        onChange={ handleFloorTextureTypeSwitch }
-      />
-      <label htmlFor="floor_texture_type__texture">Image</label>
+          <select
+            disabled={ floorTextureType !== 'image' }
+            value={ cellProperties.textureConfig.name }
+            onChange={ handleFloorImageChange }
+          >
+            <option value="default">Default</option>
+            {
+              textureList.map((texture, i) => (
+                <option value={ texture } key={ i }>{ texture }</option>
+              ))
+            }
+          </select>
+        </form>
+      </div>
       
-      {/* <input type="number" min="0" max="5" name="floor_texture" value={ floorFormState.texture }></input> */}
-
       <h4>Ceiling</h4>
-      {/* ceiling toggle */}
-      {/* */}
-      {/* <label htmlFor="ceiling_texture">Color</label>
-      <input type="number" min="0" max="5" name="ceiling_texture" value={ floorFormState.ceiling.texture }></input> */}
-
+      <input 
+        name="ceilingConfig" 
+        type="checkbox" 
+        checked={ cellProperties.ceilingConfig || false } 
+        onChange={ handleCeilingToggle }
+      ></input>
+      { 
+        renderCeilingEditor(cellProperties.ceilingConfig)
+      }
     </div>
+
   )
 };
 export default FloorEditor;
